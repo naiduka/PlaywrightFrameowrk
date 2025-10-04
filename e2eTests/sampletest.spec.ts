e3043425778b8dbd72947585767bd88030eb5d5a
naiduka <naidu1621@gmail.com>
2025-10-04 15:35:04 +0100

    chages are added

--- e2eTests/sampletest.spec.ts
+++ e2eTests/sampletest.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test ("launch browser", async ({browser}) =>
 {
     const context = browser.newContext();
+    console.log("welcome to automation workd");
     
 }
-)
+);
